c429b7b351ab0a3c0a0e1bd849639bd6c4f85434
Andrew Perrault <andrew.perrault@yahoo.ca>
2015-11-28 14:02:37 -0500

    Basic movement works.

--- HoloArena/Assets/Scripts/PlayerController.cs
+++ HoloArena/Assets/Scripts/PlayerController.cs
@@ -6,12 +6,15 @@ public class PlayerController : MonoBehaviour
     public float ForwardForce;
     public float SidewayForce;
     public float TurnSpeed;
+    public float ForwardAccel;
+    public float SidewaysAccel;
     public float MaxSpeed;
-    public float SqrMaxSpeed;
+    public float MaxBackSpeed;
+    public float MaxSidewaysSpeed;
+    public float Deceleration;
     public Vector3 LookAt;
     public float MouseSpeedHorizontal;
     public float MouseSpeedVertical;
-    public float ResponseModifier;
     public Transform LookAtTarget;
     public Transform HeadPosition;
     public float MaxTilt;
@@ -25,7 +28,6 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         RB = GetComponent<Rigidbody>();
-        SqrMaxSpeed = MaxSpeed * MaxSpeed;
         LookAt = transform.forward;
     }
 
@@ -37,26 +39,85 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Calculate movement force
-        Vector3 MoveForce = Vector3.zero;
-        MoveForce += Input.GetAxis("Vertical") * ForwardForce * transform.forward * Time.fixedDeltaTime;
-        MoveForce += Input.GetAxis("Horizontal") * SidewayForce * transform.right * Time.fixedDeltaTime;
-
-        // Limit top speed;
-        Vector3 vel = RB.velocity;
-        Vector2 xzVel = new Vector2(vel.x, vel.z);
-        if (xzVel.sqrMagnitude > SqrMaxSpeed)
+        float forward = Input.GetAxis("Vertical");
+        float sideways = Input.GetAxis("Horizontal");
+        Vector2 moveAccel = Vector2.zero;
+        if (forward > 0)
         {
-            xzVel = xzVel.normalized * MaxSpeed;
-            RB.velocity = new Vector3(xzVel.x, vel.y, xzVel.y);
+            moveAccel.y = forward * ForwardAccel;
+        }
+        else
+        {
+            moveAccel.y = forward * ForwardAccel;
+        }
+        moveAccel.x = sideways * SidewaysAccel;
+
+        Vector3 originalVel = RB.velocity;
+        Vector3 originalRelativeVel = transform.InverseTransformDirection(originalVel);
+        Vector3 newRelativeVelocity = originalRelativeVel;
+        newRelativeVelocity.z += moveAccel.y * Time.fixedDeltaTime;
+        newRelativeVelocity.x += moveAccel.x * Time.fixedDeltaTime;
+
+        // Limit forward and back
+        if (newRelativeVelocity.z > MaxSpeed)
+        {
+            newRelativeVelocity.z = MaxSpeed;
+        }
+        else if (newRelativeVelocity.z < -MaxBackSpeed)
+        {
+            newRelativeVelocity.z = -MaxBackSpeed;
+        }
+        // Limit Sideways
+        if (Mathf.Abs(newRelativeVelocity.x) > MaxSidewaysSpeed)
+        {
+            newRelativeVelocity.x = MaxSidewaysSpeed * Mathf.Sign(newRelativeVelocity.x);
+        }
+        // Limit moving on angles
+        Vector2 horizontalVelocity = new Vector2(newRelativeVelocity.x, newRelativeVelocity.z);
+        if (Vector2.SqrMagnitude(horizontalVelocity) > MaxSpeed * MaxSpeed)
+        {
+            horizontalVelocity = horizontalVelocity.normalized * MaxSpeed;
+            newRelativeVelocity.x = horizontalVelocity.x;
+            newRelativeVelocity.z = horizontalVelocity.y;
+        }
+        // Decelerate forward
+        if (forward < 0.7f && forward >= 0f)
+        {
+            newRelativeVelocity.z -= (1 - forward) * Deceleration * Time.fixedDeltaTime;
+            if (newRelativeVelocity.z < 0f)
+            {
+                newRelativeVelocity.z = 0f;
+            }
+        }
+        else if (forward > -0.7f && forward < 0f)
+        {
+            newRelativeVelocity.z += (1 - forward) * Deceleration * Time.fixedDeltaTime;
+            if (newRelativeVelocity.z > 0f)
+            {
+                newRelativeVelocity.z = 0f;
+            }
+        }
+        // Decelerate sideways
+        if (sideways < 0.7f && sideways >= 0f)
+        {
+            newRelativeVelocity.x -= (1 - sideways) * Deceleration * Time.fixedDeltaTime;
+            if (newRelativeVelocity.x < 0f)
+            {
+                newRelativeVelocity.x = 0f;
+            }
+        }
+        else if (sideways > -0.7f && sideways < 0f)
+        {
+            newRelativeVelocity.x += (1 - sideways) * Deceleration * Time.fixedDeltaTime;
+            if (newRelativeVelocity.x > 0f)
+            {
+                newRelativeVelocity.x = 0f;
+            }
         }
 
-        // Apply Movement force
-        if (xzVel.sqrMagnitude < ResponseModifier * ResponseModifier)
-        {
-            MoveForce = MoveForce + MoveForce * (ResponseModifier - xzVel.magnitude);
-        }
-        RB.AddForce(MoveForce, ForceMode.Force);
+        Debug.Log(newRelativeVelocity);
+        Vector3 newVel = transform.TransformDirection(newRelativeVelocity);
+        RB.AddForce(newVel - originalVel, ForceMode.VelocityChange);
 
         // Look Rotation
         float mouseYAngle = Input.GetAxis("Mouse X") * MouseSpeedHorizontal;
