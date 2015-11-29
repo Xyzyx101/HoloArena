c1840595eb7b89cff6f597ce3486bba165d0e269
Andrew Perrault <andrew.perrault@yahoo.ca>
2015-11-29 14:33:45 -0500

    Some raw art added.  More Movement improvements.  Look IK and Aim IK works.

---
+++ HoloArena/Assets/Scripts/IKController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+
+public class IKController : MonoBehaviour
+{
+
+    protected Animator MyAnimator;
+
+    public bool IKActive = false;
+    public bool AimRight = false;
+    public bool AimLeft = false;
+    public Transform TargetObj = null;
+    public float AimSpeed = 1f;
+    private float AimRightTimer;
+    private float AimLeftTimer;
+
+    void Start()
+    {
+        MyAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    //a callback for calculating IK
+    void OnAnimatorIK()
+    {
+        if (MyAnimator)
+        {
+            //if the IK is active, set the position and rotation directly to the goal. 
+            if (IKActive)
+            {
+
+                // Set the look target position, if one has been assigned
+                if (TargetObj != null)
+                {
+                    MyAnimator.SetLookAtWeight(1);
+                    MyAnimator.SetLookAtPosition(TargetObj.position);
+                }
+
+                // Set the right hand target position and rotation, if one has been assigned
+                if (AimRight)
+                {
+                    AimRightTimer = Mathf.Clamp(AimRightTimer + Time.deltaTime, 0f, AimSpeed);
+                    MyAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, AimRightTimer / AimSpeed);
+                    MyAnimator.SetIKPosition(AvatarIKGoal.RightHand, TargetObj.position);
+                }
+                else
+                {
+                    AimRightTimer = Mathf.Clamp(AimRightTimer - Time.deltaTime * 0.35f, 0f, AimSpeed);
+                    MyAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, AimRightTimer / AimSpeed);
+                    MyAnimator.SetIKPosition(AvatarIKGoal.RightHand, TargetObj.position);
+                }
+
+                if (AimLeft)
+                {
+                    AimLeftTimer = Mathf.Clamp(AimLeftTimer + Time.deltaTime, 0f, AimSpeed);
+                    MyAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, AimLeftTimer / AimSpeed);
+                    MyAnimator.SetIKPosition(AvatarIKGoal.LeftHand, TargetObj.position);
+                }
+                else
+                {
+                    AimLeftTimer = Mathf.Clamp(AimLeftTimer - Time.deltaTime * 0.35f, 0f, AimSpeed);
+                    MyAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, AimLeftTimer / AimSpeed);
+                    MyAnimator.SetIKPosition(AvatarIKGoal.LeftHand, TargetObj.position);
+                }
+
+            }
+
+            //if the IK is not active, set the position and rotation of the hand and head back to the original position
+            else
+            {
+                MyAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+                MyAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+                MyAnimator.SetLookAtWeight(0);
+            }
+        }
+    }
+}
--- HoloArena/Assets/Scripts/PlayerController.cs
+++ HoloArena/Assets/Scripts/PlayerController.cs
@@ -24,33 +24,44 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody RB;
     private Animator MyAnimator;
+    private IKController MyIKController;
 
     // Use this for initialization
     void Start()
     {
         RB = GetComponent<Rigidbody>();
         MyAnimator = GetComponentInChildren<Animator>();
+        MyIKController = GetComponentInChildren<IKController>();
         LookAt = transform.forward;
     }
 
     // Update is called once per frame
     void Update()
     {
-        AnimatorStateInfo debugInfo = MyAnimator.GetCurrentAnimatorStateInfo(0);
-        if (debugInfo.IsName("Idle"))
-        {
-            //Debug.Log("Idle");
-        }
+        Vector3 touchRayOrigin = transform.position + transform.up * 0.2f;
+        Vector3 touchRayDirection = -transform.up;
+        float touchRayRange = 0.85f;
+        bool touchGround = Physics.Raycast(touchRayOrigin, touchRayDirection, touchRayRange);
+        Debug.DrawRay(touchRayOrigin, touchRayDirection * touchRayRange, Color.red);
 
-        bool touchGround = Physics.Raycast(transform.position + transform.up * 0.1f, -transform.up, 0.6f);
-        Debug.DrawRay(transform.position + transform.up * 0.1f, -transform.up * 0.6f, Color.red);
+        // Attack IK
+        MyIKController.AimLeft = Input.GetButton("Fire1");
+        MyIKController.AimRight = Input.GetButton("Fire2");
+
         //// Animation Parameters
-        if (Grounded && !touchGround)
+        if (Grounded)
         {
-            Debug.Log("Fall Called");
-            MyAnimator.SetTrigger("Fall");
+            if (!touchGround)
+            {
+                MyAnimator.SetTrigger("Fall");
+            }
+            RB.drag = 2.5f;
         }
-       Grounded = touchGround;
+        else
+        {
+            RB.drag = 0f;
+        }
+        Grounded = touchGround;
         MyAnimator.SetBool("Grounded", Grounded);
 
 
@@ -72,93 +83,110 @@ public class PlayerController : MonoBehaviour
         MyAnimator.SetFloat("Strafe", relativeVel.x / MaxStrafeSpeed);
 
         // Turn
-        float angle = MathUtils.SignedVectorAngle(transform.forward, LookAt, transform.up);
+        Vector3 projLookAt = MathUtils.ProjectVectorOnPlane(Vector3.up, LookAt);
+        float angle = MathUtils.SignedVectorAngle(transform.forward, projLookAt, transform.up);
         MyAnimator.SetFloat("Turn", Mathf.Clamp(angle / 90, -1f, 1f));
+        //Debug.Log(angle / 90f);
 
         // AnimationSpeed
         MyAnimator.SetFloat("Speed", 0.8f + RB.velocity.magnitude * 0.2f);
+
+        //AnimatorStateInfo debugInfo = MyAnimator.GetCurrentAnimatorStateInfo(0);
+        //if (debugInfo.IsName("Idle"))
+        //{
+        //    Debug.Log("Idle");
+        //}
+        //else if (debugInfo.IsName("TurnOnSpot"))
+        //{
+        //    Debug.Log("TurnOnSpot");
+        //    Debug.Log(angle / 90f);
+        //}
     }
 
     void FixedUpdate()
     {
-        float forward = Input.GetAxis("Vertical");
-        float sideways = Input.GetAxis("Horizontal");
-        Vector2 moveAccel = Vector2.zero;
-        if (forward > 0)
+        if (Grounded)
         {
-            moveAccel.y = forward * ForwardAccel;
-        }
-        else
-        {
-            moveAccel.y = forward * ForwardAccel;
-        }
-        moveAccel.x = sideways * SidewaysAccel;
+            float forward = Input.GetAxis("Vertical");
+            float sideways = Input.GetAxis("Horizontal");
+            Vector2 moveAccel = Vector2.zero;
+            if (forward > 0)
+            {
+                moveAccel.y = forward * ForwardAccel;
+            }
+            else
+            {
+                moveAccel.y = forward * ForwardAccel;
+            }
+            moveAccel.x = sideways * SidewaysAccel;
 
-        Vector3 originalVel = RB.velocity;
-        Vector3 originalRelativeVel = transform.InverseTransformDirection(originalVel);
-        Vector3 newRelativeVelocity = originalRelativeVel;
-        newRelativeVelocity.z += moveAccel.y * Time.fixedDeltaTime;
-        newRelativeVelocity.x += moveAccel.x * Time.fixedDeltaTime;
+            Vector3 originalVel = RB.velocity;
+            Vector3 originalRelativeVel = transform.InverseTransformDirection(originalVel);
+            Vector3 newRelativeVelocity = originalRelativeVel;
+            newRelativeVelocity.z += moveAccel.y * Time.fixedDeltaTime;
+            newRelativeVelocity.x += moveAccel.x * Time.fixedDeltaTime;
 
-        // Limit forward and back
-        if (newRelativeVelocity.z > MaxSpeed)
-        {
-            newRelativeVelocity.z = MaxSpeed;
-        }
-        else if (newRelativeVelocity.z < -MaxBackSpeed)
-        {
-            newRelativeVelocity.z = -MaxBackSpeed;
-        }
-        // Limit Sideways
-        if (Mathf.Abs(newRelativeVelocity.x) > MaxStrafeSpeed)
-        {
-            newRelativeVelocity.x = MaxStrafeSpeed * Mathf.Sign(newRelativeVelocity.x);
-        }
-        // Limit moving on angles
-        Vector2 horizontalVelocity = new Vector2(newRelativeVelocity.x, newRelativeVelocity.z);
-        if (Vector2.SqrMagnitude(horizontalVelocity) > MaxSpeed * MaxSpeed)
-        {
-            horizontalVelocity = horizontalVelocity.normalized * MaxSpeed;
-            newRelativeVelocity.x = horizontalVelocity.x;
-            newRelativeVelocity.z = horizontalVelocity.y;
-        }
-        // Decelerate forward
-        if (forward < 0.7f && forward >= 0f)
-        {
-            newRelativeVelocity.z -= (1 - forward) * Deceleration * Time.fixedDeltaTime;
-            if (newRelativeVelocity.z < 0f)
+            // Limit forward and back
+            if (newRelativeVelocity.z > MaxSpeed)
             {
-                newRelativeVelocity.z = 0f;
+                newRelativeVelocity.z = MaxSpeed;
             }
-        }
-        else if (forward > -0.7f && forward < 0f)
-        {
-            newRelativeVelocity.z += (1 - forward) * Deceleration * Time.fixedDeltaTime;
-            if (newRelativeVelocity.z > 0f)
+            else if (newRelativeVelocity.z < -MaxBackSpeed)
             {
-                newRelativeVelocity.z = 0f;
+                newRelativeVelocity.z = -MaxBackSpeed;
             }
-        }
-        // Decelerate sideways
-        if (sideways < 0.7f && sideways >= 0f)
-        {
-            newRelativeVelocity.x -= (1 - sideways) * Deceleration * Time.fixedDeltaTime;
-            if (newRelativeVelocity.x < 0f)
+            // Limit Sideways
+            if (Mathf.Abs(newRelativeVelocity.x) > MaxStrafeSpeed)
             {
-                newRelativeVelocity.x = 0f;
+                newRelativeVelocity.x = MaxStrafeSpeed * Mathf.Sign(newRelativeVelocity.x);
             }
-        }
-        else if (sideways > -0.7f && sideways < 0f)
-        {
-            newRelativeVelocity.x += (1 - sideways) * Deceleration * Time.fixedDeltaTime;
-            if (newRelativeVelocity.x > 0f)
+            // Limit moving on angles
+            Vector2 horizontalVelocity = new Vector2(newRelativeVelocity.x, newRelativeVelocity.z);
+            if (Vector2.SqrMagnitude(horizontalVelocity) > MaxSpeed * MaxSpeed)
             {
-                newRelativeVelocity.x = 0f;
+                horizontalVelocity = horizontalVelocity.normalized * MaxSpeed;
+                newRelativeVelocity.x = horizontalVelocity.x;
+                newRelativeVelocity.z = horizontalVelocity.y;
             }
-        }
 
-        Vector3 newVel = transform.TransformDirection(newRelativeVelocity);
-        RB.AddForce(newVel - originalVel, ForceMode.VelocityChange);
+            // Decelerate forward
+            if (forward < 0.7f && forward >= 0f)
+            {
+                newRelativeVelocity.z -= (1 - forward) * Deceleration * Time.fixedDeltaTime;
+                if (newRelativeVelocity.z < 0f)
+                {
+                    newRelativeVelocity.z = 0f;
+                }
+            }
+            else if (forward > -0.7f && forward < 0f)
+            {
+                newRelativeVelocity.z += (1 - forward) * Deceleration * Time.fixedDeltaTime;
+                if (newRelativeVelocity.z > 0f)
+                {
+                    newRelativeVelocity.z = 0f;
+                }
+            }
+            // Decelerate sideways
+            if (sideways < 0.7f && sideways >= 0f)
+            {
+                newRelativeVelocity.x -= (1 - sideways) * Deceleration * Time.fixedDeltaTime;
+                if (newRelativeVelocity.x < 0f)
+                {
+                    newRelativeVelocity.x = 0f;
+                }
+            }
+            else if (sideways > -0.7f && sideways < 0f)
+            {
+                newRelativeVelocity.x += (1 - sideways) * Deceleration * Time.fixedDeltaTime;
+                if (newRelativeVelocity.x > 0f)
+                {
+                    newRelativeVelocity.x = 0f;
+                }
+            }
+
+            Vector3 newVel = transform.TransformDirection(newRelativeVelocity);
+            RB.AddForce(newVel - originalVel, ForceMode.VelocityChange);
+        }
 
         // Look Rotation
         float mouseYAngle = Input.GetAxis("Mouse X") * MouseSpeedHorizontal;
