832549c03c9ea86110376488e6b7ede67cd82c10
Andrew Perrault <andrew.perrault@yahoo.ca>
2015-11-28 22:07:44 -0500

    Turning and falling works.

--- HoloArena/Assets/Scripts/PlayerController.cs
+++ HoloArena/Assets/Scripts/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
     public float SidewaysAccel;
     public float MaxSpeed;
     public float MaxBackSpeed;
-    public float MaxSidewaysSpeed;
+    public float MaxStrafeSpeed;
     public float Deceleration;
     public Vector3 LookAt;
     public float MouseSpeedHorizontal;
@@ -19,22 +19,64 @@ public class PlayerController : MonoBehaviour
     public Transform HeadPosition;
     public float MaxTilt;
     public float MinTilt;
-
     public Quaternion turnRotation;
+    public bool Grounded;
 
     private Rigidbody RB;
+    private Animator MyAnimator;
 
     // Use this for initialization
     void Start()
     {
         RB = GetComponent<Rigidbody>();
+        MyAnimator = GetComponentInChildren<Animator>();
         LookAt = transform.forward;
     }
 
     // Update is called once per frame
     void Update()
     {
+        AnimatorStateInfo debugInfo = MyAnimator.GetCurrentAnimatorStateInfo(0);
+        if (debugInfo.IsName("Idle"))
+        {
+            //Debug.Log("Idle");
+        }
 
+        bool touchGround = Physics.Raycast(transform.position + transform.up * 0.1f, -transform.up, 0.6f);
+        Debug.DrawRay(transform.position + transform.up * 0.1f, -transform.up * 0.6f, Color.red);
+        //// Animation Parameters
+        if (Grounded && !touchGround)
+        {
+            Debug.Log("Fall Called");
+            MyAnimator.SetTrigger("Fall");
+        }
+       Grounded = touchGround;
+        MyAnimator.SetBool("Grounded", Grounded);
+
+
+        // Forward
+        Vector3 relativeVel = transform.InverseTransformDirection(RB.velocity);
+        if (relativeVel.z == 0f)
+        {
+            MyAnimator.SetFloat("Forward", 0);
+        }
+        else if (relativeVel.z > 0)
+        {
+            MyAnimator.SetFloat("Forward", relativeVel.z / MaxSpeed);
+        }
+        else if (relativeVel.z < 0)
+        {
+            MyAnimator.SetFloat("Forward", relativeVel.z / MaxBackSpeed);
+        }
+        // Strafe
+        MyAnimator.SetFloat("Strafe", relativeVel.x / MaxStrafeSpeed);
+
+        // Turn
+        float angle = MathUtils.SignedVectorAngle(transform.forward, LookAt, transform.up);
+        MyAnimator.SetFloat("Turn", Mathf.Clamp(angle / 90, -1f, 1f));
+
+        // AnimationSpeed
+        MyAnimator.SetFloat("Speed", 0.8f + RB.velocity.magnitude * 0.2f);
     }
 
     void FixedUpdate()
@@ -68,9 +110,9 @@ public class PlayerController : MonoBehaviour
             newRelativeVelocity.z = -MaxBackSpeed;
         }
         // Limit Sideways
-        if (Mathf.Abs(newRelativeVelocity.x) > MaxSidewaysSpeed)
+        if (Mathf.Abs(newRelativeVelocity.x) > MaxStrafeSpeed)
         {
-            newRelativeVelocity.x = MaxSidewaysSpeed * Mathf.Sign(newRelativeVelocity.x);
+            newRelativeVelocity.x = MaxStrafeSpeed * Mathf.Sign(newRelativeVelocity.x);
         }
         // Limit moving on angles
         Vector2 horizontalVelocity = new Vector2(newRelativeVelocity.x, newRelativeVelocity.z);
@@ -115,7 +157,6 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        Debug.Log(newRelativeVelocity);
         Vector3 newVel = transform.TransformDirection(newRelativeVelocity);
         RB.AddForce(newVel - originalVel, ForceMode.VelocityChange);
 
